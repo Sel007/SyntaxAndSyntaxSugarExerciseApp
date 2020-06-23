6e0cefc25645fc26a7955d5e59c7b89fc8d02797
Sel007 <selina.08225.1@gmail.com>
2020-06-23 11:29:58 -0400

    Add project files.

---
+++ SyntaxAndSyntaxSugarExerciseConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SyntaxAndSyntaxSugarExerciseConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            var answer = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
